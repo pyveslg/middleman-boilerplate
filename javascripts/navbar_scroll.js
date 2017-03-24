2edfa61589895bbc2afa6a6a4e5fd56049d1a18d
pyves.lg@gmail.com <pyves.lg@gmail.com>
2017-03-24 18:17:20 +0100

    Automated commit at 2017-03-24 17:17:18 UTC by middleman-deploy 1.0.0

---
+++ javascripts/navbar_scroll.js
@@ -0,0 +1,22 @@
+// Navbar transition on scroll
+$(function(){
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 50) {
+      $(".navbar").css({
+        "background-color": "white",
+      });
+      $(".navbar li a").css({
+        "color": "#0096B3",
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+      });
+      $(".navbar li a").css({
+        "color": "white",
+      });
+    }
+  });
+})
+;
